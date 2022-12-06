4d9e529781356f994e7b83075f99abd4b6697238
carled7 <carlos_ed03@outlook.com>
2022-12-06 16:59:26 -0300

    added comments

--- src/entities/libraries/libraries.service.ts
+++ src/entities/libraries/libraries.service.ts
@@ -17,17 +17,21 @@ export class LibrariesService {
   async addFavorite(data: AddToLibraryDto) {
     let movieIsKnown = false;
 
+    //checks if the movie selected is already in the database
     await this.libMoviesService
       .has(data.imdbId.toString())
       .then((value) => (movieIsKnown = value));
 
     if (movieIsKnown)
+      //if it exists, the relation user-movie is create in the libraries table
       return this.libraryRepository.save(this.libraryRepository.create(data));
     else {
+      //if it doesn`t, it is added to table moviesInLibrary table ...
       await this.moviesApiService
         .getMovieById(data.imdbId.toString())
         .then((movie) => this.libMoviesService.save(movie));
 
+      //...then, its relation with the user is created in the libraries table
       return this.libraryRepository.save(this.libraryRepository.create(data));
     }
   }
